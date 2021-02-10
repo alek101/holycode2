96b210de29d9b5a527568ffafb89cc87da2a4aca
alek101 <47416711+alek101@users.noreply.github.com>
2021-02-10 14:58:41 +0100

    Autocompleate
    :)

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,33 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import postRequest from './postRequest';
+import { Hint } from 'react-autocomplete-hint';
 
 const Post = () => {
     const url='http://localhost:8000/authorList';
     const [author, setAuthor] = useState('');
+    const [hintData, setHintData] = useState('');
     let data={author: author};
+
+    const getDataHint = async (url) => {
+        const rawResponse = await fetch(url);
+        const response = await rawResponse.json();
+        let hintArray=response.map(a=>a.author);
+        setHintData(hintArray);
+    }
+
+
+    useEffect(()=>{
+        getDataHint(url);
+    },[])
 
     return ( 
 
         <div className="form">
-            <input name="author" id="author"
-            value={author} onChange={(e) => setAuthor(e.target.value)}></input>
+            <Hint options={hintData} allowTabFill>
+                <input name="author" id="author"
+                    value={author} onChange={(e) => setAuthor(e.target.value)} />
+            </Hint>
+
             <button onClick={(e)=>postRequest(url,data)}>Send</button>
         </div>
      );
